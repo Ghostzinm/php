39440a8c9e2fab520f7bdd87af9e43187f191cf7
Ghost <100890062+Ghostzinm@users.noreply.github.com>
2025-08-19 08:32:01 -0300

    php

--- aula_02/index.php
+++ aula_02/index.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/style.css">
-    <title>Formulario</title>
-</head>
 
-<body>
+<?php 
+
+include("./template/header.php");
+
+?>
+
     <section class="container">
         <h1 class="mb-5">Formulario</h1>
         <main>
